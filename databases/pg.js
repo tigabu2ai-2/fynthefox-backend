49b1a97312bc136b54568ee5e7709d87b875c4e5
Eyob Mesfin <dailydeveloper51437@gmail.com>
2025-09-13 18:29:49 +0300

    changed some env variabale names

--- databases/pg.js
+++ databases/pg.js
@@ -1,9 +1,9 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
-const DB_NAME = process.env.DB_NAME;
-const DB_USER = process.env.DB_USER;
-const DB_PASSWORD = process.env.DB_PASSWORD;
-const DB_HOST = process.env.DB_HOST;
+const DB_NAME = process.env.PG_DB_NAME;
+const DB_USER = process.env.PG_DB_USER;
+const DB_PASSWORD = process.env.PG_DB_PASSWORD;
+const DB_HOST = process.env.PG_DB_HOST;
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     host: DB_HOST,
     dialect: 'postgres',
