a4df9fb735637a3d4a9ae0c37df63949c0ea8fe4
Eyob Mesfin <dailydeveloper51437@gmail.com>
2025-09-15 19:23:26 +0300

    changed the refresh-token endpoint from POST to GET. And applied middleware for validating the refresh-token

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const AuthController = require('../controllers/auth_controller');
 const AuthValidator = require('../validators/auth_validator');
+const authenticateRefreshToken = require('../middlewares/authenticate_refresh_token')
 
 const router = express.Router();
 
 router.post('/login', AuthValidator.validateLogin, AuthController.login);
 
-router.post('/refreshToken', AuthValidator.validateRefreshToken, AuthController.refreshToken);
+router.get('/refresh-token', AuthValidator.validateRefreshToken, authenticateRefreshToken, AuthController.refreshToken);
 
 router.post('/forgot-password', AuthValidator.validateForgotPassword, AuthController.forgotPassword);
 
