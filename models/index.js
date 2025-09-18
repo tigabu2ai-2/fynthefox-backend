a99b97536307364637cb37a7df0ece9c55847bcb
Eyob Mesfin <dailydeveloper51437@gmail.com>
2025-09-18 22:42:22 +0300

    defined model associations

--- models/index.js
+++ models/index.js
@@ -5,6 +5,11 @@ const Address = require('./address');
 const Subscription = require('./subscription');
 const SubscriptionPlan = require('./subscription_plan');
 const TenantInfo = require('./tenant_info');
+const Complaint = require('./complaint')
+const ComplaintLog = require('./complaint_log')
+const Agent = require('./agent')
+const ChannelPreference = require('./channel_preference')
+const VendorInfo = require('./vendor_info')
 
 Address.hasOne(Property, { foreignKey: 'address_id', onDelete: 'SET NULL' });
 Property.belongsTo(Address, { foreignKey: 'address_id' });
@@ -25,6 +30,38 @@ TenantInfo.hasOne(User, { foreignKey: 'tenant_info_id', onDelete: 'SET NULL' });
 User.belongsTo(TenantInfo, { foreignKey: 'tenant_info_id' });
 
 
+
+// Creating association between resident and complaint
+User.hasMany(Complaint, {as:'Complaints', foreignKey: 'user_id', onDelete: 'CASCADE' })
+Complaint.belongsTo(User, { as:'Complainant',foreignKey: 'user_id' })
+
+//Creating association between vendor and complaint
+User.hasMany(Complaint, { as: 'AssinedWorkOrders', foreignKey: 'assigned_to', onDelete: 'SET NULL' })
+Complaint.belongsTo(User, { as: 'Vendor', foreignKey: 'assigned_to' })
+
+// Creating association between property and complaint
+Property.hasMany(Complaint, { foreignKey: 'property_id', onDelete: 'CASCADE' })
+Complaint.belongsTo(Property, { foreignKey: 'property_id' })
+
+
+// Creating association between complaint and complain_log
+Complaint.hasMany(ComplaintLog, { foreignKey: 'complaint_id', onDelete: 'CASCADE' })
+ComplaintLog.belongsTo(Complaint)
+
+
+
+// Creating association between User (Property Owner) and AI Agent
+User.hasOne(Agent, { foreignKey: 'owner_id', onDelete: 'CASCADE' })
+Agent.belongsTo(User, { foreignKey: 'owner_id' })
+
+// Creating associatio between Agent and Channel Preference
+Agent.hasOne(ChannelPreference, { foreignKey: 'agent_id', onDelete: 'CASCADE' })
+ChannelPreference.belongsTo(Agent, { foreignKey: 'agent_id' })
+
+// Creating association between user and vendor-info
+VendorInfo.hasOne(User, { foreignKey: 'vendor_info_id', onDelete: 'SET NULL' })
+User.belongsTo(VendorInfo, { foreignKey: 'vendor_info_id' })
+
 module.exports = {
     User,
     Role,
@@ -33,4 +70,9 @@ module.exports = {
     Subscription,
     SubscriptionPlan,
     TenantInfo,
+    VendorInfo,
+    Complaint,
+    ComplaintLog,
+    Agent,
+    ChannelPreference
 };
