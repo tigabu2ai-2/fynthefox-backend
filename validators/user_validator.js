abe69bdcebef2c814a6f56f89810dd2a5560178c
Eyob Mesfin <dailydeveloper51437@gmail.com>
2025-09-18 22:49:28 +0300

    updated the validator for vendro registration

--- validators/user_validator.js
+++ validators/user_validator.js
@@ -9,7 +9,7 @@ class UserValidator {
             email: Joi.string().email().required(),
             password: Joi.string().pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$')).message('Password must contain at least 8 characters, including uppercase, lowercase, number and special character').required(),
             phone_number: Joi.string().pattern(/^[0-9]{9,15}$/).required(),
-            
+
         })
         const { error } = schema.validate(req.body);
         if (error) {
@@ -18,29 +18,33 @@ class UserValidator {
         next();
     }
 
-      static validateVendorRegistration(req, res, next) {
+    static validateVendorRegistration(req, res, next) {
         const schema = Joi.object({
             first_name: Joi.string().min(2).max(30).required(),
             last_name: Joi.string().min(2).max(30).required(),
             email: Joi.string().email().required(),
             password: Joi.string().pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$')).message('Password must contain at least 8 characters, including uppercase, lowercase, number and special character').required(),
             phone_number: Joi.string().pattern(/^[0-9]{9,15}$/).required(),
-            
+            type: Joi.string().valid(...Object.values(['plumber', 'electrician'])).required(),
+            priority: Joi.number().optional(),
+            availability: Joi.object().optional()
+
         })
         const { error } = schema.validate(req.body);
         if (error) {
+            console.log(error)
             return ResponseBuilder.validationError(error.details.map(d => d.message)).send(res);
         }
         next();
     }
-      static validatePropertyOwnerRegistration(req, res, next) {
+    static validatePropertyOwnerRegistration(req, res, next) {
         const schema = Joi.object({
             first_name: Joi.string().min(2).max(30).required(),
             last_name: Joi.string().min(2).max(30).required(),
             email: Joi.string().email().required(),
             password: Joi.string().pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$')).message('Password must contain at least 8 characters, including uppercase, lowercase, number and special character').required(),
             phone_number: Joi.string().pattern(/^[0-9]{9,15}$/).required(),
-            
+
         })
         const { error } = schema.validate(req.body);
         if (error) {
@@ -49,7 +53,7 @@ class UserValidator {
         next();
     }
 
-      static validatePropertyUserRegistration(req, res, next) {
+    static validatePropertyUserRegistration(req, res, next) {
         const schema = Joi.object({
             first_name: Joi.string().min(2).max(30).required(),
             last_name: Joi.string().min(2).max(30).required(),
