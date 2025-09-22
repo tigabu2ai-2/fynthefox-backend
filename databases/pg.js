1b72f73c85e478b2471905ca5842ad648435b8ec
Eyob Mesfin <dailydeveloper51437@gmail.com>
2025-09-22 19:51:54 +0300

    changed the main database from postgres to mysql just for development phase, it will be changd back to postgres for production

--- databases/pg.js
+++ databases/pg.js
@@ -1,25 +1,16 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize")
+const mysql = require('mysql2')
+
 require('dotenv').config();
 const DB_NAME = process.env.PG_DB_NAME;
 const DB_USER = process.env.PG_DB_USER;
 const DB_PASSWORD = process.env.PG_DB_PASSWORD;
 const DB_HOST = process.env.PG_DB_HOST;
+
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     host: DB_HOST,
-    dialect: 'postgres',
-    logging: false
-});
+    dialect: 'mysql',
+    dialectModule: mysql
+})
 
-async function connectDB() {
-    try { 
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-        
-    } catch (error) { 
-        console.error('Unable to connect to the database:', error);
-    }
-}
-
-connectDB();
-
-module.exports = sequelize;
+module.exports = sequelize
