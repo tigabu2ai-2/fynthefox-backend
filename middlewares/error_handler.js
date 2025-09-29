ae07804d36a59188ebef6620ea75977742eb1c3e
Eyob Mesfin <dailydeveloper51437@gmail.com>
2025-09-29 10:55:15 +0300

    created a middleware for handling unhandled erros before sending to users

---
+++ middlewares/error_handler.js
@@ -0,0 +1,12 @@
+const Logger = require("../utils/logger")
+const ResponseBuilder = require("../utils/response_builder")
+
+const logger = new Logger("ErrorHandler")
+
+function errorHandler(err, req,res,next){
+    logger.error("Unhandled Error", err);
+
+    return ResponseBuilder.serverError().send()
+} 
+
+module.exports = errorHandler
