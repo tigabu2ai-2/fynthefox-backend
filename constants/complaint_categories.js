ee29d1e6ad14ed7156cd683258937a85acc6f6fe
Eyob Mesfin <dailydeveloper51437@gmail.com>
2025-09-30 10:57:48 +0300

    created the list for complaint categories

---
+++ constants/complaint_categories.js
@@ -0,0 +1,23 @@
+const ComplaintCategories = [
+"plumbing",
+"electrical",
+"hvac",
+"water-supply",
+"sewage",
+"structural",
+"pest-control",
+"cleaning",
+"elevator",
+"security",
+"noise",
+"parking",
+"waste-management",
+"internet",
+"common-area",
+"landscaping",
+"gas",
+"fire-safety",
+"other"    
+]
+
+module.exports = ComplaintCategories
