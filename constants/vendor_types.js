021ae881bfe3e6d5a7da889d091a4fa7b55a542e
Eyob Mesfin <dailydeveloper51437@gmail.com>
2025-09-30 11:26:33 +0300

    added new vendor types to the list

--- constants/vendor_types.js
+++ constants/vendor_types.js
@@ -1,1 +1,25 @@
-const Types = ["electrician", "plumber"]
+const Types = [
+    "electrician", 
+    "plumber",
+    "hvac-technician",
+    "water-supply-specialist",
+    "sewage_specialit",
+    "mason",
+    "pest-control-service",
+    "cleaning-service",
+    "elevator-technician",
+    "security-service",
+    "soundproofing-contractor",
+    "parking-attendant",
+    "waste-management-service",
+    "internet-provider",
+    "facility-maintenance",
+    "landscaper",
+    "lighting-technician",
+    "gas-technician",
+    "fire-safety-inspector",
+    "general-contractor",
+    "other"
+]
+
+module.exports = Types
