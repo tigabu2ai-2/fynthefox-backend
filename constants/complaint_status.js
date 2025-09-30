c7dffbd71358b176ea5fca7b4ea5a7c12d074151
Eyob Mesfin <dailydeveloper51437@gmail.com>
2025-09-30 17:54:10 +0300

    created a new constant file to store complaint status list

---
+++ constants/complaint_status.js
@@ -0,0 +1,12 @@
+const ComplaintStatus = [
+    'pending', 
+    'assigned', 
+    'scheduled', 
+    'in-progress', 
+    'estimate-needed', 
+    'resident-confirmation', 
+    'pending-vendor-acceptance', 
+    'completed'
+]
+
+module.exports = ComplaintStatus
