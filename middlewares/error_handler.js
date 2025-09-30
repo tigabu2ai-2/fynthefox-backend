39cdc1fb6f400d74b1fff2dd80d8c834c4e0f2b4
Eyob Mesfin <dailydeveloper51437@gmail.com>
2025-09-30 11:59:09 +0300

    fixed some typo

--- middlewares/error_handler.js
+++ middlewares/error_handler.js
@@ -6,7 +6,7 @@ const logger = new Logger("ErrorHandler")
 function errorHandler(err, req,res,next){
     logger.error("Unhandled Error", err);
 
-    return ResponseBuilder.serverError().send()
+    return ResponseBuilder.serverError().send(res)
 } 
 
 module.exports = errorHandler
