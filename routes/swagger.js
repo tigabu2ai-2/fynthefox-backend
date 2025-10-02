c5f8e6e74a82493ecc07220b0e121a5346b58808
Eyob Mesfin <dailydeveloper51437@gmail.com>
2025-10-02 18:53:49 +0300

    changed the swagger file name

--- routes/swagger.js
+++ routes/swagger.js
@@ -5,7 +5,7 @@ const YAML = require('yamljs')
 
 const router = express.Router()
 
-const swagger_document = YAML.load('./fynthefox.yaml')
+const swagger_document = YAML.load('./swagger.yaml')
 
 router.use('/', swaggerUi.serve, swaggerUi.setup(swagger_document))
 
