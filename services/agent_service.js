86bf324dc0abda3e85ff3a91245c366af0798760
Eyob Mesfin <dailydeveloper51437@gmail.com>
2025-10-12 23:30:02 +0300

    removed try catch block from agent service methods

--- services/agent_service.js
+++ services/agent_service.js
@@ -3,6 +3,8 @@ const { Agent, ChannelPreference, Property, User, TenantInfo, VendorInfo, Compla
 const crypto = require('crypto')
 const bcrypt = require('bcrypt')
 
+const Logger = require('../utils/logger')
+const logger = new Logger('AgentService')
 require('dotenv').config()
 
 class AgentService {
@@ -10,41 +12,36 @@ class AgentService {
         this.agent_api_key_hash_secret = process.env.AGENT_API_KEY_HASH_SECRET
     }
     async create(data) {
-        try {
-            if (await Agent.findOne({ where: { company_info_id: data.company_info_id } })) {
-                throw new CustomException('Company already have an AI Agent!', 400)
-            }
-            const uuid = crypto.randomUUID() // This will be shared to the user
-            const agent_api_key = crypto.createHmac("sha256", this.agent_api_key_hash_secret).update(uuid).digest('hex') // This will be stored in the DB
 
-            const channels = ["voice", "email", "whatsapp", "web_form"]
+        if (await Agent.findOne({ where: { company_info_id: data.company_info_id } })) {
+            throw new CustomException('Company already have an AI Agent!', 400)
+        }
+        const uuid = crypto.randomUUID() // This will be shared to the user
+        const agent_api_key = crypto.createHmac("sha256", this.agent_api_key_hash_secret).update(uuid).digest('hex') // This will be stored in the DB
 
-            const channel_preferences = Object.fromEntries(channels.map(channel => [channel, channel === data.channel_preference]))
-            const agent = await Agent.create({
-                company_info_id: data.company_info_id,
-                status: 'active',
-                language: data.language,
-                api_key: agent_api_key,
-                n8n_complaint_webhook_url:data.n8n_complaint_webhook_url,
-                n8n_user_webhook_url:data.n8n_user_webhook_url,
-                ChannelPreference: {
-                    ...channel_preferences
-                }
-            }, {
-                include: [{ model: ChannelPreference }]
-            })
-            if (!agent || agent == null) {
-                throw new CustomException('Failed to create agent! Please try again.', 500)
-            }
+        const channels = ["voice", "email", "whatsapp", "web_form"]
 
-            agent.api_key = uuid;
-            return agent
-        } catch (e) {
-            if (e instanceof CustomException) {
-                throw e
+        const channel_preferences = Object.fromEntries(channels.map(channel => [channel, channel === data.channel_preference]))
+        const agent = await Agent.create({
+            company_info_id: data.company_info_id,
+            status: 'active',
+            language: data.language,
+            api_key: agent_api_key,
+            n8n_complaint_webhook_url: data.n8n_complaint_webhook_url,
+            n8n_user_webhook_url: data.n8n_user_webhook_url,
+            ChannelPreference: {
+                ...channel_preferences
             }
+        }, {
+            include: [{ model: ChannelPreference }]
+        })
+        if (!agent || agent == null) {
             throw new CustomException('Failed to create agent! Please try again.', 500)
         }
+
+        agent.api_key = uuid;
+        return agent
+
     }
 
     async isAgentExist(agent_id) {
@@ -52,33 +49,28 @@ class AgentService {
     }
 
     async get_property_id(agent_id, user_id) {
-        try {
-            const agent = await Agent.findByPk(agent_id,)
-            if (!agent || agent == null) {
-                throw new CustomException('Agent not found!', 400)
-            }
-            const user = await User.findByPk(user_id, {
-                include: [
-                    {
-                        model: TenantInfo,
-                        as: "TenantInfo",
-                        required: true,
-                        include: [{
-                            model: Property,
-                            required: true,
-                            where: { company_info_id: agent.company_info_id },
-                        }]
-                    }
-                ]
-            })
-            if (!user || user.TenantInfo.Property == null) {
-                throw new CustomException('Property not found!', 400)
-            }
-            return user.TenantInfo.Property.id;
-
-        } catch (e) {
-            throw new CustomException('Failed to fetch property! Please try again', 500)
+        const agent = await Agent.findByPk(agent_id,)
+        if (!agent || agent == null) {
+            throw new CustomException('Agent not found!', 400)
         }
+        const user = await User.findByPk(user_id, {
+            include: [
+                {
+                    model: TenantInfo,
+                    as: "TenantInfo",
+                    required: true,
+                    include: [{
+                        model: Property,
+                        required: true,
+                        where: { company_info_id: agent.company_info_id },
+                    }]
+                }
+            ]
+        })
+        if (!user || user.TenantInfo.Property == null) {
+            throw new CustomException('Property not found!', 400)
+        }
+        return user.TenantInfo.Property.id;
     }
 
     async get_agent_by_api_key(api_key) {
@@ -94,7 +86,7 @@ class AgentService {
     }
 
     async generate_agent_api_key(agent_id) {
-        try {
+        
             const agent = await Agent.findByPk(agent_id)
             if (!agent || agent == null) {
                 throw new CustomException('Agent not found!', 400)
@@ -105,12 +97,7 @@ class AgentService {
             agent.api_key = agent_api_key
             await agent.save()
             return uuid
-        } catch (e) {
-            if (e instanceof CustomException) {
-                throw e
-            }
-            throw new CustomException('Failed to generate API Key! Please try again', 500)
-        }
+      
     }
 
     async is_agent_of(agent_id, user_id, user_role) {
