397c72461a79d3429ed0828d6589c1fe28509cc2
Eyob Mesfin <dailydeveloper51437@gmail.com>
2026-01-06 01:25:38 +0300

    implemented Estimate endpoint

--- app.js
+++ app.js
@@ -1,74 +1,84 @@
-const express = require('express');
-require('dotenv').config();
-const cors = require('cors')
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const multer = require("multer");
 
-const Logger = require("./utils/logger")
-const logger = new Logger("Bootstrap")
+const Logger = require("./utils/logger");
+const logger = new Logger("Bootstrap");
 
-const errorHandler = require("./middlewares/error_handler")
+const errorHandler = require("./middlewares/error_handler");
 
+const sequelize = require("./databases/pg");
+const agent_sequelize = require("./databases/agent_mysql");
 
-const sequelize = require('./databases/pg');
-const agent_sequelize = require('./databases/agent_mysql')
+const createRoles = require("./seeders/create_roles");
+const createSuperAdmin = require("./seeders/create_super_admin");
 
-const createRoles = require('./seeders/create_roles')
-const createSuperAdmin = require('./seeders/create_super_admin')
-
-const authRoutes = require('./routes/auth');
-const userRoutes = require('./routes/user');
-const propertiesRoutes = require('./routes/property');
-const agentRoutes = require('./routes/agent')
-const complaintRoutes = require('./routes/complaint')
-const accountRoutes = require('./routes/account')
-const dashboardRoutes = require('./routes/dashboard');
-
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const propertiesRoutes = require("./routes/property");
+const agentRoutes = require("./routes/agent");
+const complaintRoutes = require("./routes/complaint");
+const accountRoutes = require("./routes/account");
+const dashboardRoutes = require("./routes/dashboard");
+const estimateRoutes = require("./routes/estimate");
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 //Allow all origins
-app.use(cors())
+app.use(cors());
 
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/properties', propertiesRoutes);
-app.use('/api/agents', agentRoutes)
-app.use('/api/complaints', complaintRoutes)
-app.use('/api/account', accountRoutes)
-app.use('/api/dashboard', dashboardRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/properties", propertiesRoutes);
+app.use("/api/agents", agentRoutes);
+app.use("/api/complaints", complaintRoutes);
+app.use("/api/account", accountRoutes);
+app.use("/api/dashboard", dashboardRoutes);
+app.use("/api/estimates", estimateRoutes);
 
-if (process.env.NODE_ENV === 'development') {
-    const swaggerDocRoutes = require("./routes/swagger")
-    app.use('/api/docs', swaggerDocRoutes)
+app.use((err, req, res, next) => {
+  if (err instanceof multer.MulterError) {
+    return res.status(400).json({ message: err.message });
+  }
+  next(err);
+});
+
+app.use(errorHandler);
+
+if (process.env.NODE_ENV === "development") {
+  const swaggerDocRoutes = require("./routes/swagger");
+  app.use("/api/docs", swaggerDocRoutes);
 }
 
-app.use(errorHandler)
-
 async function initializeApp() {
-    try {
-        await sequelize.sync({ alter: false , force: false});
-        logger.info('Database synced');
+  try {
+    await sequelize.sync({ alter: false, force: false });
+    logger.info("Database synced");
 
-        await agent_sequelize.sync()
+    await agent_sequelize.sync();
 
-        // Seed roles if they don't exist
-        await createRoles();
+    // Seed roles if they don't exist
+    await createRoles();
 
-        // Seed super admin user if it doesn't exist
-        await createSuperAdmin();
+    // Seed super admin user if it doesn't exist
+    await createSuperAdmin();
 
-        app.listen(PORT, () => {
-            logger.info(`Server is running on port ${PORT}`);
-        }).on('error', (err) => {
-            logger.error('Server error:', err);
-        })
-    } catch (e) {
-        logger.error('Error during app initialization:', e);
-        process.exit(1);
-    }
+    app
+      .listen(PORT, () => {
+        logger.info(`Server is running on port ${PORT}`);
+      })
+      .on("error", (err) => {
+        logger.error("Server error:", err);
+      });
+  } catch (e) {
+    logger.error("Error during app initialization:", e);
+    process.exit(1);
+  }
 }
 
 initializeApp();
